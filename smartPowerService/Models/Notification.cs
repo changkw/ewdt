ae42585cebd815ba86d317c1841e091686deab9b
txwong <tzexiangwong@gmail.com>
2018-02-01 16:22:59 +0800

    Add files via upload

---
+++ smartPowerService/Models/Notification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace smartPowerService.Models
+{
+    public class Notification
+    {
+        public int NotificationID { get; set; }
+        [Required]
+        public DateTime timeDate { get; set; }
+        public int Id { get; set; }
+        [ForeignKey("Id")]
+        public CurrentActivity CurrentActivity { get; set; }
+
+
+
+
+
+    }
+
+
+
+
+
+}
