4d08e160d929e5f3c90d45c5a0b9543ff93c701b
txwong <tzexiangwong@gmail.com>
2018-02-01 16:21:51 +0800

    Add files via upload

---
+++ smartPowerService/Controllers/CurrentActivitiesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using smartPowerService.Models;
+
+namespace smartPowerService.Controllers
+{
+    public class CurrentActivitiesController : ApiController
+    {
+        private smartPowerServiceContext db = new smartPowerServiceContext();
+
+        // GET: api/CurrentActivities
+        public IQueryable<CurrentActivity> GetCurrentActivities()
+        {
+            return db.CurrentActivities;
+        }
+
+        // GET: api/CurrentActivities/5
+        [ResponseType(typeof(CurrentActivity))]
+        public async Task<IHttpActionResult> GetCurrentActivity(int id)
+        {
+            CurrentActivity currentActivity = await db.CurrentActivities.FindAsync(id);
+            if (currentActivity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(currentActivity);
+        }
+
+        // PUT: api/CurrentActivities/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCurrentActivity(int id, CurrentActivity currentActivity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != currentActivity.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(currentActivity).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CurrentActivityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CurrentActivities
+        [ResponseType(typeof(CurrentActivity))]
+        public async Task<IHttpActionResult> PostCurrentActivity(CurrentActivity currentActivity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CurrentActivities.Add(currentActivity);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = currentActivity.Id }, currentActivity);
+        }
+
+        // DELETE: api/CurrentActivities/5
+        [ResponseType(typeof(CurrentActivity))]
+        public async Task<IHttpActionResult> DeleteCurrentActivity(int id)
+        {
+            CurrentActivity currentActivity = await db.CurrentActivities.FindAsync(id);
+            if (currentActivity == null)
+            {
+                return NotFound();
+            }
+
+            db.CurrentActivities.Remove(currentActivity);
+            await db.SaveChangesAsync();
+
+            return Ok(currentActivity);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CurrentActivityExists(int id)
+        {
+            return db.CurrentActivities.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ smartPowerService/Controllers/NotificationsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using smartPowerService.Models;
+
+namespace smartPowerService.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        private smartPowerServiceContext db = new smartPowerServiceContext();
+
+        // GET: api/Notifications
+        public IQueryable<Notification> GetNotifications()
+        {
+            return db.Notifications;
+        }
+
+        // GET: api/Notifications/5
+        [ResponseType(typeof(Notification))]
+        public async Task<IHttpActionResult> GetNotification(int id)
+        {
+            Notification notification = await db.Notifications.FindAsync(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(notification);
+        }
+
+        // PUT: api/Notifications/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutNotification(int id, Notification notification)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != notification.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(notification).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NotificationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Notifications
+        [ResponseType(typeof(Notification))]
+        public async Task<IHttpActionResult> PostNotification(Notification notification)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Notifications.Add(notification);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (NotificationExists(notification.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = notification.Id }, notification);
+        }
+
+        // DELETE: api/Notifications/5
+        [ResponseType(typeof(Notification))]
+        public async Task<IHttpActionResult> DeleteNotification(int id)
+        {
+            Notification notification = await db.Notifications.FindAsync(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            db.Notifications.Remove(notification);
+            await db.SaveChangesAsync();
+
+            return Ok(notification);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool NotificationExists(int id)
+        {
+            return db.Notifications.Count(e => e.Id == id) > 0;
+        }
+    }
+}
